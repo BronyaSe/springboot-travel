8617c209942aa683904136e52f8a8d95ac8b504e
Bronya <1282933745@qq.com>
2025-02-27 19:23:28 +0800

    清理缓存

--- src/main/java/com/bronya/travel/Utils/MD5Util.java
+++
@@ -1,9 +0,0 @@
-package com.bronya.travel.Utils;
-
-import org.apache.commons.codec.digest.DigestUtils;
-
-public class MD5Util {
-     public static String encrypt(String data) {
-         return DigestUtils.md5Hex(data);
-     }
-}
